77a0badfc78d7a2de5ec7826aded97b71e7a782b
lizhaohui3637 <932593814@qq.com>
2019-03-14 09:27:52 +0800

    haha

---
+++ Test/src/_01/demo.java
@@ -0,0 +1,23 @@
+package _01;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		c a=new c("");
+	}
+}
+class c extends c1{
+	public c() {
+		super.c="3";
+	}
+	public c(String a) {
+		
+	}
+}
+class c1{
+	String c="2";
+	public c1() {
+		System.out.println(c);
+	}
+}
